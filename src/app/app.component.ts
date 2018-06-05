54cba578d7ece70a28645cb86d04abe2e61c0f6e
yonatan-ramirez <yonatan.ramirez557@gmail.com>
2018-06-05 16:52:49 -0700

    practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+class Greeting {
+  message: string; 
+  constructor(message:string){
+this.message = message;
+  }
+  greet (){
+    return "Hello, " + this.message;
+  }
+
+}
 export class AppComponent {
-  title = 'app';
+// People(){
+// function lastname(){
+//   if() = true{
+//     let male = 
+//   }
+// }
+
+// }
+
 }
