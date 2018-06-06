3bca542ae3839e7bab6fd2d38d5001ce76a64921
yonatan-ramirez <yonatan.ramirez557@gmail.com>
2018-06-06 16:24:38 -0700

    Person-gender

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
+class Person{
+  // name: string;
+  // rating: number;
+  // gender: string;
+  constructor(public name: string, public gender: string, public rating: number){
+
+  }
+
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-class Greeting {
-  message: string; 
-  constructor(message:string){
-this.message = message;
-  }
-  greet (){
-    return "Hello, " + this.message;
-  }
+export class AppComponent implements OnInit{
+ female = new Person('jane Doe', 'f', 2);
+ male = new Person('john Doe', 'm', 4);
 
-}
-export class AppComponent {
+ person;
 
+ ngOnInit(){
+  this.pickRandomPerson();
+ }
+
+ pickRandomPerson(){
+  const index = Math.floor(Math.random() * 2 );
+  if(!index){
+    this.person = this.female;
+  }else{
+    this.person= this.male;
+  }
+  console.log(this.person);
+ }
+ getName(){
+   return `Name: ${this.person.name}`;
+ }
+ switchPerson(){
+   if (this.person.gender === 'f'){
+     this.person.gender = 'm';
+   }else if(this.person.gender==='m'){
+     this.person= this.female;
+   }else{
+     this.pickRandomPerson();
+   }
+
+  }
 }
