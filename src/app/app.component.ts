b3fa9795ccb83a13dc0142363586c9df1dc41afb
yonatan-ramirez <yonatan.ramirez557@gmail.com>
2018-06-06 11:36:03 -0700

    random-button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,25 @@ import {  } from '../app/warning-message/warning-message';
 export class AppComponent {
   title = ' My app';
 
+  componentSelected: boolean = false;
+  // selectedComponent;
+  index: number;
+  
+  selectComponent(){
+    this.componentSelected = true;
+    this.index = Math.floor(Math.random() + 3);
+    console.log('index', this.index);
+  //   if(index ===0){
+  //     // this.selectedComponent= 'warning';
+  //     console.log('display warning message component ');
+  //   }else if( index ===1 ){
+  //     // this.selectedComponent = 'success';
+  //     console.log('display success message component');
+
+  //   }else if (index===2 ){
+  //     // this.selectedComponent='danger';
+  //     console('display danger message component');
+  //   }
+  }
+
 }
